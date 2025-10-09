72fa20933a26c526c966bc45d1b8664760d38f32
Mohamed Larabi <molarabi007@gmail.com>
2025-10-09 16:51:51 +0000

    api key projet : AIzaSyCXzix8LlOluEuB9ognN3c4eISggKkngME project id : s

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,12 +1,10 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
 import { firebaseConfig } from "./config";
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
 const db = getFirestore(app);
-const storage = getStorage(app);
 
-export { app, auth, db, storage };
+export { app, auth, db };
