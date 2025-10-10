083a5764828baec6a0af39bba0ab5277a963549a
Mohamed Larabi <molarabi007@gmail.com>
2025-10-10 16:57:42 +0000

    Ce site est inaccessible localhost n'autorise pas la connexion. Voici qu

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -17,5 +17,5 @@ export async function GET(request: Request) {
   }
 
   // return the user to an error page with instructions
-  return NextResponse.redirect(`${origin}/?error=Could not process authentication`)
+  return NextResponse.redirect(`${origin}/auth?error=Could not process authentication`)
 }
