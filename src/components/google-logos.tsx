678b70213c8c62c8d87e6a64568ab89fbd12ef01
Mohamed Larabi <molarabi007@gmail.com>
2025-11-25 14:02:05 +0000

    c'est le vrai logo (_for element path_)

--- src/components/google-logos.tsx
+++ src/components/google-logos.tsx
@@ -12,16 +12,16 @@ export const GmailLogo = () => (
 );
 
 export const GoogleDriveLogo = () => (
-    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" className="h-8 w-8">
-        <path fill="#1aa260" d="m21.328 22l5.333-9.333h-10.66L10.667 22Z"/>
-        <path fill="#34a853" d="m5.333 12.667l5.334-9.334l5.333 9.334l-5.333 9.333Z"/>
-        <path fill="#fcc934" d="m16 22l-5.333-9.333h16L21.328 22Z"/>
-        <path fill="#0066da" d="m10.667 3.333l10.666 18.667l5.334-9.333L16 3.333Z"/>
+    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 28" className="h-7 w-8">
+        <path fill="#1aa260" d="M21.328 28l5.333-9.333H10.66L5.328 28Z"/>
+        <path fill="#34a853" d="M5.333 18.667L0 9.333l10.667-9.333L16 9.333Z"/>
+        <path fill="#fcc934" d="m16 9.333l5.333-9.333h10.667l-5.333 9.333Z"/>
+        <path fill="#0066da" d="m32 9.333l-5.333 9.334l-10.667-9.334L21.333 0Z"/>
     </svg>
 );
 
 export const YouTubeLogo = () => (
-    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 28 20" className="h-5 w-5">
+    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 28 20" className="h-5 w-7">
         <path fill="#FF0000" d="M27.5 5.5s-.2-1.8-.9-2.6c-.9-1-2.2-1-2.7-1C20.4 1.8 14 1.8 14 1.8s-6.4 0-9.9.1c-.5 0-1.8.1-2.7 1-.7.8-.9 2.6-.9 2.6S.2 7.4.2 9.3v1.4c0 1.9.2 3.8.2 3.8s.2 1.8.9 2.6c.9 1 2.2 1 2.7 1.1 3.5.1 9.9.1 9.9.1s6.4 0 9.9-.1c.5-.1 1.8-.1 2.7-1.1.7-.8.9-2.6.9-2.6s.2-1.9.2-3.8v-1.4c0-1.9-.2-3.8-.2-3.8z"/>
         <path fill="#FFFFFF" d="M11.2 13.4V6.6l6.8 3.4-6.8 3.4z"/>
     </svg>
